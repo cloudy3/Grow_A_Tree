437ea201b54fff32f9115fb2c9e6d96422d45f66
cloudy3 <cjingfeng98@gmail.com>
2021-03-22 18:39:55 +0800

    Updated frontend

--- growatree/main/static/main/js/map.js
+++ growatree/main/static/main/js/map.js
@@ -3,11 +3,12 @@
 var map = null;
 var infowindow = null;
 var markers = [];
-var mapState = "ewaste";
 
 var GOOGLE_MAPS_API_KEY = "AIzaSyBnZtv1xAxEgTHcme4vJTF2SBU0f06FmqQ";
 
 var src = "https://geo.data.gov.sg/ewaste/2021/02/19/kml/ewaste.kml";
+var cash_for_trash_src =
+  "https://geo.data.gov.sg/cashfortrash/2019/02/27/kml/cashfortrash.kml";
 
 // Defaults to centre of Singapore
 var defaultPosition = {
@@ -17,37 +18,6 @@ var defaultPosition = {
   },
 };
 
-// Switches to E-Waste data after user clicks on E-waste button
-function eWaste() {
-  src = "https://geo.data.gov.sg/ewaste/2021/02/19/kml/ewaste.kml";
-  console.log("Switched to E-Waste data!");
-  initMap();
-  mapState = "ewaste";
-}
-
-// Switches to Cash-For-Trash data after user clicks on Cash-For-Trash button
-function cashForTrash() {
-  src = "https://geo.data.gov.sg/cashfortrash/2019/02/27/kml/cashfortrash.kml";
-  console.log("Switched to Cash-For-Trash data!");
-  initMap();
-  mapState = "cashfortrash";
-}
-
-function lightingWaste() {
-  src = "https://geo.data.gov.sg/lighting/2019/10/01/kml/lighting.kml";
-  console.log("Switched to Lighting-Waste data!");
-  initMap();
-  mapState = "lightingwaste";
-}
-
-function secondHandGoods() {
-  src =
-    "https://geo.data.gov.sg/secondhandcollecn/2017/11/30/kml/secondhandcollecn.kml";
-  console.log("Switched to Second Hand Goods data!");
-  initMap();
-  mapState = "secondhandgoods";
-}
-
 // Initialises Google Map
 function initMap() {
   // Get user's location
@@ -55,7 +25,7 @@ function initMap() {
   x.getCurrentPosition(success, failure);
 
   function success(position) {
-    console.log("Loaded Successfully!");
+    console.log("\n\n\nLoaded Successfully!");
 
     var userLat = position.coords.latitude;
     var userLong = position.coords.longitude;
@@ -69,15 +39,6 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    // Add user's location as a marker
-    const image =
-      "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-    const userMarker = new google.maps.Marker({
-      position: { lat: userLat, lng: userLong },
-      map,
-      icon: image,
-    });
-
     // Adds all the markers from data.gov
     var kmlLayer = new google.maps.KmlLayer(src, {
       suppressInfoWindows: false,
@@ -85,56 +46,24 @@ function initMap() {
       map: map,
     });
 
-    kmlLayer.addListener("click", function (kmlEvent) {
+    kmlLayer.addListener('click', function(kmlEvent) {
       // Access the description of each marker
       var text = kmlEvent.featureData.description;
 
       // Tags to be queried
-      const BUILDING = "<th>ADDRESSBUILDINGNAME</th>";
-      const BLOCK = "<th>ADDRESSBLOCKHOUSENUMBER</th>";
-      const STREET = "<th>ADDRESSSTREETNAME</th>";
-      const POSTAL = "<th>ADDRESSPOSTALCODE</th>";
-      const UNIT = "<th>ADDRESSUNITNUMBER</th>";
+      const BUILDING = "<th>ADDRESSBUILDINGNAME</th>"
+      const BLOCK = "<th>ADDRESSBLOCKHOUSENUMBER</th>"
 
       // Index of Location Name
-      // TODO:Check if building name is ""
-      var startIndex;
-      var endIndex;
-      var location
-
-      if (mapState.localeCompare("ewaste") == 0) {
-        startIndex = text.search(BUILDING);
-        endIndex = text.search(BLOCK);
-        location = text.slice(startIndex + 32, endIndex - 27);
-      } else if (mapState.localeCompare("cashfortrash") == 0) {
-        startIndex = text.search(STREET);
-        endIndex = text.search(POSTAL);
-        location = text.slice(startIndex + 30, endIndex - 27);
-      } else if (mapState.localeCompare("lightingwaste") == 0) {
-        startIndex = text.search(BUILDING);
-        endIndex = text.search(UNIT);
-        location = text.slice(startIndex + 33, endIndex - 27);
-      } else if (mapState.localeCompare("secondhandgoods") == 0) {
-        startIndex = text.search(STREET);
-        endIndex = text.search(POSTAL);
-        location = text.slice(startIndex + 30, endIndex - 27);
-      } else {
-        location = "";
-        console.log("Error getting location of marker!");
-      }
-      console.log(location);
-      // updateButtons(location);
+      const startIndex = text.search(BUILDING);
+      const endIndex = text.search(BLOCK);
+      var address = text.slice(startIndex+32, endIndex-27);
+      console.log(address);
     });
   }
 
   // If map fails to load
   function failure() {
-    console.log("Failed to load!");
+    console.log("\n\n\nFailed to load!");
   }
-}
-
-// Updates Button Text to be the Location name
-function updateButtons(location) {
-  var locationText = document.getElementById("myButton1");
-  locationText.value = location;
 }
