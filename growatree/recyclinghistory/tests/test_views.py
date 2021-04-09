2fd084501dadfbc0ae0944745fcccb1711e8775e
beaniestanley <beanbeanstanley@gmail.com>
2021-04-09 14:32:23 +0800

    Add test_views

---
+++ growatree/recyclinghistory/tests/test_views.py
@@ -0,0 +1,56 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.utils import timezone
+from ..models import RecyclingEntry
+
+
+class RecyclingHistoryTestView(TestCase):
+
+    def setUp(self):
+        # Create user
+        test_user1 = User.objects.create_user(username='testuser1', password='(jnFe42i')
+        test_user1.save()
+
+        self.client = Client()
+        self.recycling_history_url = reverse('recycling-history')
+      
+    def test_logged_in_recyling_history_GET(self):
+        login = self.client.login(username='testuser1', password='(jnFe42i')
+        response = self.client.get(self.recycling_history_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recyclinghistory/recyclinghistory.html')
+
+
+class RecyclingEntryTestView(TestCase):
+  
+    def setUp(self):
+        # Create user
+        test_user1 = User.objects.create_user(username='testuser1', password='(jnFe42i')
+        test_user1.save()
+        
+        self.recyclingentry_create_url = reverse('recyclingentry_create')
+        self.RecyclingEntry1 = RecyclingEntry.objects.create(
+            user=test_user1,
+            date=timezone.now(),
+            location='NTU',
+            recyclingType='Plastic',
+            recyclingWeight=10
+        )
+
+    def test_logged_in_recyling_entry_GET(self):
+        login = self.client.login(username='testuser1', password='(jnFe42i')
+        response = self.client.get(self.recyclingentry_create_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recyclinghistory/recyclingentry_form.html')
+
+    def test_create_recyling_entry_POST(self):
+        response = self.client.post(self.recyclingentry_create_url, {
+            'recyclingType': 'Plastic',
+            'recyclingWeight': 10
+        })
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(self.RecyclingEntry1.recyclingType, 'Plastic')
+        self.assertEquals(self.RecyclingEntry1.recyclingWeight, 10)
+
